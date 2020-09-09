0927ac7d750faf62876e80646680ac8b9f5fb840
Odin <odin@debian>
2020-09-09 10:23:29 +1000

    Autotest

---
+++ Printing/helloworld.py
@@ -0,0 +1,9 @@
+# Write code that prints 'Hello World!' followed by a newline char
+import sys
+
+def helloWorld():
+    #Write your code here.
+    return None # Replace this line. It is simply a placeholder
+    # Instead of the usual print(...) use 'return ...', this just helps with autotesting
+
+print(helloWorld())
---
+++ Printing/inputOutput.py
@@ -0,0 +1,11 @@
+# Write code that reads input (users name) and prints the output in the form 'Hello <user>!'
+#       where <user> is the input received
+
+def inputoutput(user_in):
+
+    # Instead of the usual print(...) use 'return ...', this just helps with autotesting
+    # This is where you need to write your code in the form as shown below
+    return user_in # Change this line
+    
+def printio():
+    print(inputoutput(None)) # Change 'None' to the user input
